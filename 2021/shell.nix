ce50e3e8c7248a7ecc6b9ce0b6cb271623474ce2
Markus Hauck <markus1189@gmail.com>
2021-12-02 08:28:56 +0100

    Add update script

--- 2021/shell.nix
+++ 2021/shell.nix
@@ -3,9 +3,21 @@ with pkgs;
 
 let
   range = lib.range 0 2;
-  compileHaskell = day: inputFile: pkgs.runCommand "aoc-builder" {} ''
-    mkdir -p $out/bin
-    ${myGhc}/bin/ghc --make -O3 -rtsopts -eventlog -threaded -o "$out/bin/d${day}-compiled" ${inputFile}
+  compileHaskell = day: inputFile:
+    pkgs.runCommand "aoc-builder" { } ''
+      mkdir -p $out/bin
+      ${myGhc}/bin/ghc --make -O3 -rtsopts -eventlog -threaded -o "$out/bin/d${day}-compiled" ${inputFile}
+    '';
+  updateScript = pkgs.writeShellScriptBin "update" ''
+    set -e
+
+    DAY="$(${pkgs.coreutils}/bin/date +%d)"
+    DAY_NO_ZERO="$(${pkgs.coreutils}/bin/date +%-d)"
+    echo "Updating for day $DAY_NO_ZERO"
+    cp -n -v day00.hs "day''${DAY}.hs"
+    sed -i -e "s/^\([[:space:]]*\)range = lib.range 0 [[:digit:]]\+/\1range = lib.range 0 ''${DAY_NO_ZERO}/" shell.nix
+
+    echo Done
   '';
   myHaskellPackages = ps:
     with ps; [
@@ -41,4 +53,7 @@ let
     in writeScriptBin "d${n'}" ''
       ${myGhc}/bin/runhaskell --ghc-arg=-Wall day${n'}.hs
     '';
-in mkShell { buildInputs = [ myGhc ] ++ map mkDay range ++ map compileDay range; }
+in mkShell {
+  buildInputs = [ myGhc updateScript ] ++ map mkDay range
+    ++ map compileDay range;
+}
